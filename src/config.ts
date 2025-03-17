154a642b25971ef67a1314c83a27713019d84797
eeem77 <escobarmernestoe@gmail.com>
2025-03-17 14:21:29 -0400

    first commit

---
+++ src/config.ts
@@ -0,0 +1,66 @@
+
+
+export const SITE_FAVICON = "/logo.png";
+export const SITE_LOGO = "/leo.png";
+export const SITE_TITLE = "WEEKEND JUMPERS";
+export const SITE_URL = "https://weekendjumpers.com";
+export const SITE_DESCRIPTION = `
+  At Weekend Jumpers, we are dedicated to creating magical and unforgettable moments for children. Specializing in the rental of inflatable bounce houses, we transform parties and events into experiences filled with fun, laughter, and joy. Our products, designed with the highest standards of quality and safety, are meant to inflate not just bounce houses, but also hearts. With a professional approach and a touch of magic, we ensure that every event is unique and special. Because at Weekend Jumpers, we believe that children's smiles are the greatest gift, and we are here to make them shine. Turn your celebration into a memory that lasts forever!
+`;
+export const ME_AVATAR = "/logo.png";
+export const LANGUAGES = ['en'];
+
+export const MENUS = [
+  {
+    title: " Contact Us",
+    link: "/me",
+    target: "_self",
+    icon: "",
+  },
+];
+
+export const FOOTER_CONTENT = "weekendjumpers. All rights reserved.";
+
+export const CONTACTS = {
+  linkedIn: "YOUR_LINKEDIN_URL",
+  github: "YOUR_GITHUB_URL",
+  email: "mailto:YOUR_EMAIL",
+};
+
+export const SLOGAN = "Fun that fills the heart, smiles that last forever.";
+export const ME_DESCRIPTION = `At Weekend Jumpers, we are dedicated to creating magical and unforgettable moments for children. Specializing in the rental of inflatable bounce houses, we transform parties and events into experiences filled with fun, laughter, and joy. Our products, designed with the highest standards of quality and safety, are meant to inflate not just bounce houses, but also hearts. With a professional approach and a touch of magic, we ensure that every event is unique and special. Because at Weekend Jumpers, we believe that children's smiles are the greatest gift, and we are here to make them shine. Turn your celebration into a memory that lasts forever!`;
+
+
+export const WORK_EXPERIENCE = [
+  {
+    company: "(408) 551 - 8916",
+    position: "",
+    startDate: "2022",
+    endDate: "2024",
+  },
+
+  {
+    company: "weekendjumperss@gmail.com",
+    position: "",
+    startDate: "2023",
+    endDate: "2023",
+  },
+];
+
+
+export const PROJECTS = [
+    {
+      id: "my-first-rocket-launch",
+      title: "My First Rocket Launch",
+      description: "This is the first rocket launch I participated in. I was a member of the Interstellar Academy Program, and I was responsible for the rocket's navigation system. I was able to successfully navigate the rocket to the target destination.",
+      image: 'https://images.unsplash.com/photo-1516849841032-87cbac4d88f7?q=80&w=2370&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      externalLink: "https://www.nasa.gov/",
+      details: [
+        "Explaining space exploration concepts through creative illustrations.",
+        "Making it easier for aspiring explorers to understand the basics of interstellar travel.",
+        "Won an award in the interstellar creativity competition.",
+      ],
+    },
+  ];
+  
+export const GOOGLE_GTAG = "";
